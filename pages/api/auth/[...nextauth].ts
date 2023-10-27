85c79e59f9e602dee8fdc0cb59abdd4cffd7095f
Steven Clouston <stevenclouston@gmail.com>
2023-10-27 13:22:30 +1300

    Remove unused code

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -22,30 +22,7 @@ export const authOptions = {
     strategy: "jwt",
   },
 
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log({ user, account, profile, email, credentials });
-      const isAllowedToSignIn = true;
-      if (isAllowedToSignIn) {
-        return true;
-      } else {
-        // Return false to display a default error message
-        return false;
-        // Or you can return a URL to redirect to:
-        // return '/unauthorized'
-      }
-    },
-    async session({ session, token, user }: any) {
-      //hook into authsignal here
-      console.log("SESSIOn");
-      console.log({ token });
-      // Send properties to the client, like an access_token and user id from a provider.
-      session.accessToken = token?.accessToken;
-      session.user.id = token?.id;
-      console.log({ session });
-      return session;
-    },
-  },
+  callbacks: {},
   providers: [
     EmailProvider({
       server: {
